1084a6b514810575643735c2b58595c9c8598dbb
Tajammal Hussain <managebydev@gmail.com>
2025-05-22 10:27:35 +0500

    initial commint simple project

---
+++ customDropdown/customDropdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  customDropdownApp.swift
+//  customDropdown
+//
+//  Created by Ml Bench on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct customDropdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
